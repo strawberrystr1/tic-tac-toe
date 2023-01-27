e36656698761c2406ec0660494723251986a7ff1
Andrei Shakh <andrey_overdose@mail.ru>
2023-01-27 14:46:26 +0300

    fix: fix urls and env

--- back/src/db.ts
+++ back/src/db.ts
@@ -1,10 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const dbClient = new Sequelize("tic-tac-toe", "postgres", "password", {
-  host: "localhost",
-  port: 5432,
-  dialect: "postgres",
-  logging: false
-});
+const dbClient = new Sequelize(
+  process.env.PGDATABASE as string,
+  process.env.PGUSER as string,
+  process.env.PGPASSWORD as string,
+  {
+    host: process.env.PGHOST,
+    port: +(process.env.PGPORT as string),
+    dialect: "postgres",
+    logging: false
+  }
+);
 
 export default dbClient;
